dacae843c907af0283f6dfa5a8b7f4daca783eab
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-06-30 15:31:51 +0000

    Customized admin

--- stock/admin.py
+++ stock/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from .models import Item, Category
 
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(Category)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display =(
+        'sku',
+        'name',
+        'category',
+        'price',
+        'price_hd',
+        'price_2k',
+        'price_4k',
+        'rating',
+        'location',
+        'video',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
