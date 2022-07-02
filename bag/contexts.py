597cb51e4b761a4dd4b33476220b0e1d824d5c63
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-07-02 03:36:34 +0000

    Added bag context and removed delivery

---
+++ bag/contexts.py
@@ -0,0 +1,15 @@
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    item_count = 0
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'item_count': item_count,
+    }
+
+    return context
