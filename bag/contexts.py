f0c27cf099e5397278233efaded8f294e464237b
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-07-02 04:57:44 +0000

    updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,10 +1,25 @@
-
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from stock.models import Item
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     item_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        item = get_object_or_404(Item, pk=item_id)
+        total += quantity * item.price_hd
+        item_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'item': item,
+        })
+
+    grand_total = total
 
     context = {
         'bag_items': bag_items,
