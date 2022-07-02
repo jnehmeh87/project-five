397ab8841fc5caef705c1a69194b918caa50354a
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-07-02 01:06:05 +0000

    Added sorting

--- stock/views.py
+++ stock/views.py
@@ -12,8 +12,23 @@ def all_items(request):
     items = Item.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request .GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sor = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                items = items.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            items = items.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category']. split(',')
             items = items.filter(category__name__in=categories)
@@ -28,10 +43,13 @@ def all_items(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             items = items.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'items': items,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     
     return render(request, 'items/items.html', context)
