a011364d8ab79df05b276d91b83159484d4b5a82
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-07-04 17:57:32 +0000

    removed secret key and set debug

--- ader_films/settings.py
+++ ader_films/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4t0zyy09v)436k1+fyb211&n^xbo7pn+3=@e9@b)bnro29$nt@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ader-films.herokuapp.com', 'localhost']
 
