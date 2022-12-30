815989a0c18c488747ac9635bce6cbe9cce0674f
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-12-30 16:13:42 +0000

    Added checkout views and templates

--- checkout/admin.py
+++ checkout/admin.py
@@ -15,8 +15,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     fields = ('order_number', 'date', 'full_name',
                 'email', 'phone_number', 'country',
-                'postcode', 'town_or_city','street_address_1',
-                'street_address_2', 'county', 'order_total',
+                'postcode', 'town_or_city','street_address1',
+                'street_address2', 'county', 'order_total',
                 'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
