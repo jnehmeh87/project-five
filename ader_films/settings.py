048650037aec2505dee1c27806b7bb156dc315cb
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-12-30 19:30:53 +0000

    Added basic checkout functionality

--- ader_films/settings.py
+++ ader_films/settings.py
@@ -176,6 +176,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
