d8501d614aa9581f4edb303564ca18220432a17f
jnehmeh87 <jadnehmeh.87@gmail.com>
2023-01-03 21:37:06 +0000

    Updated allauth templates and tested profiles

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+import json
+
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
-from .forms import OrderForm
-from .models import Order, OrderLineItem
+import stripe
+
 from stock.models import Item
 from bag.contexts import bag_contents
+from .forms import OrderForm
+from .models import Order, OrderLineItem
 
-import stripe
-import json
 
 @require_POST
 def cache_checkout_data(request):
@@ -65,21 +68,24 @@ def checkout(request):
                         order_line_item.save()
                 except Item.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
+                        "One of the products in your bag wasn't found in our \
+                         database. "
                         "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(
+                request, "There's nothing in your bag at the moment")
             return redirect(reverse('items'))
 
         current_bag = bag_contents(request)
