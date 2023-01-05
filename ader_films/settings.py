f8b13677b994a8e4a4ed5a33810f1588e51715e8
jnehmeh87 <jadnehmeh.87@gmail.com>
2023-01-05 15:34:13 +0000

    updated items fixture file and loaded it to database

--- ader_films/settings.py
+++ ader_films/settings.py
@@ -121,10 +121,12 @@ WSGI_APPLICATION = 'ader_films.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
+    print('postgres')
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
+    print('sqlite')
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
