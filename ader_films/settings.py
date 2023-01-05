a43c90eae99baf0773f628ecf085779a315d0284
jnehmeh87 <jadnehmeh.87@gmail.com>
2023-01-05 23:45:10 +0000

    Added sitemap and robots to root directory

--- ader_films/settings.py
+++ ader_films/settings.py
@@ -193,7 +193,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 if 'USE_AWS' in os.environ:
     # Cache control
