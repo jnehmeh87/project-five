9583a67fb7c8555e1120d21e162cb1dd6eb5065e
jnehmeh87 <jadnehmeh.87@gmail.com>
2023-01-07 21:25:18 +0000

    Final refactoring

--- checkout/models.py
+++ checkout/models.py
@@ -13,7 +13,7 @@ from profiles.models import UserProfile
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     user_profile = models.ForeignKey(
-        UserProfile, on_delete=models.SET_NULL, null=True, blank=True, 
+        UserProfile, on_delete=models.SET_NULL, null=True, blank=True,
         related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -63,13 +63,13 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
-        Order, null=False, blank=False, on_delete=models.CASCADE, 
+        Order, null=False, blank=False, on_delete=models.CASCADE,
         related_name='lineitems')
     item = models.ForeignKey(
         Item, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False, blank=False, 
+        max_digits=6, decimal_places=2, null=False, blank=False,
         editable=False)
 
     def save(self, *args, **kwargs):
