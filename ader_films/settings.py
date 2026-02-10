7155f83dd9062a608bb9791f2884814cd46b1573
Jad Nehmeh <88036369+jnehmeh87@users.noreply.github.com>
2026-02-10 22:33:52 +0100

    Fix allauth middleware issue

--- ader_films/settings.py
+++ ader_films/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'ader_films.urls'
